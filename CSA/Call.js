9ee14d6282644dfacd561e2a785d840a61d09b69
Anudeep K K <anudeep2003@gmail.com>
2023-10-18 10:41:16 +0530

    call dialer

--- CSA/Call.js
+++ CSA/Call.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 import Communications from 'react-native-communications';
 
 const ContactPage = () => {
-    const phoneNumber = '+91 9481447196'; // Replace with your actual phone number
-
+    const [phoneNumber, setPhoneNumber] = useState('');
+    
     const handleCallPress = () => {
-        Communications.phonecall(phoneNumber, true);
+        if (phoneNumber) {
+            Communications.phonecall(phoneNumber, true);
+        }
     };
 
     return (
         <ImageBackground
-            source={{ uri: 'https://img.freepik.com/premium-photo/orange-yellow-watercolor-paper_851755-81.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696982400&semt=ais' }}
+            source={{
+                uri:
+                    'https://img.freepik.com/premium-photo/orange-yellow-watercolor-paper_851755-81.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696982400&semt=ais',
+            }}
             style={styles.container}
         >
             <View style={styles.content}>
                 <View>
-                <Text style={styles.title}>Contact Us</Text>
-                <Text style={styles.phoneNumber}>Phone: {phoneNumber}</Text>
-                <Button title="Call Now"
-                 onPress={handleCallPress}
-                 color={'orange'} />
+                    <Text style={styles.title}>Contact Us</Text>
+                    <TextInput
+                        style={styles.phoneNumberInput}
+                        placeholder="Enter Phone Number"
+                        keyboardType="phone-pad"
+                        onChangeText={text => setPhoneNumber(text)}
+                        value={phoneNumber}
+                    />
+                    <Button title="Call Now" onPress={handleCallPress} color={'orange'} />
                 </View>
             </View>
         </ImageBackground>
@@ -36,22 +45,19 @@ const styles = StyleSheet.create({
     content: {
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
         padding: 20,
-        borderRadius:10,
-    },
-    cont:{
-        flex:1,
-        flexDirection:'column',
-        justifyContent:'space-evenly',
-
+        borderRadius: 10,
     },
     title: {
         fontSize: 24,
         marginBottom: 20,
-        color:"yellow"
+        color: 'yellow',
     },
-    phoneNumber: {
-        fontSize: 18,
+    phoneNumberInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
         marginBottom: 10,
+        padding: 5,
     },
 });
 
