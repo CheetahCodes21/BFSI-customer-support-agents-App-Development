db66ef7d9630e87dbbb4ad3a0a20dbff50d2df5a
Anudeep K K <anudeep2003@gmail.com>
2023-11-22 11:40:47 +0530

    call

--- CSA/screens/Call.js
+++ CSA/screens/Call.js
@@ -1,102 +1,238 @@
-import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Modal,
+} from 'react-native';
 import Communications from 'react-native-communications';
-import Logo from '../assets/images/Logo.png';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const ContactPage = () => {
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [comment, setComment] = useState(''); // New state for comment
+const ContactPage = ({ navigation }) => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [comment, setComment] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
 
-    const handleCallPress = () => {
-        if (phoneNumber) {
-            Communications.phonecall(phoneNumber, true);
-        }
-    };
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
-    return (
-        <ImageBackground
-            source={{
-                uri:
-                    'https://img.freepik.com/premium-vector/abstract-watercolor-background_71674-1277.jpg',
-            }}
-            style={styles.container}
+  const handleCallPress = () => {
+    if (phoneNumber) {
+      Communications.phonecall(phoneNumber, true);
+    }
+  };
+
+  return (
+    <ImageBackground
+      source={{
+        uri:
+          'https://img.freepik.com/premium-vector/abstract-watercolor-background_71674-1277.jpg',
+      }}
+      style={styles.container}
+    >
+      <View style={styles.mainContainer}>
+        {/* Header */}
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Make Call</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Input Fields */}
+        <View style={styles.inputBox}>
+          <TextInput
+            style={styles.phoneNumberInput}
+            placeholder="Enter Phone Number"
+            keyboardType="phone-pad"
+            placeholderTextColor={'purple'}
+            onChangeText={(text) => setPhoneNumber(text)}
+            value={phoneNumber}
+          />
+          <TouchableOpacity onPress={handleCallPress}>
+            {/* Use MaterialCommunityIcons here */}
+            <MaterialCommunityIcons
+              name="phone"
+              size={30}
+              color="black"
+              style={styles.callIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          style={styles.commentInput}
+          placeholder="Call purpose"
+          placeholderTextColor={'purple'}
+          multiline
+          numberOfLines={6}
+          onChangeText={(text) => setComment(text)}
+          value={comment}
+        />
+
+        {/* Modal */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={toggleModal}
         >
-            <View style={styles.content}>
-                <Image source={Logo} style={styles.logo} />
-                <View>
-                    <Text style={styles.title}>Call </Text>
-                    <TextInput
-                        style={styles.phoneNumberInput}
-                        placeholder="Enter Phone Number"
-                        keyboardType="phone-pad"
-                        placeholderTextColor={"purple"}
-                        onChangeText={text => setPhoneNumber(text)}
-                        value={phoneNumber}
+          <View style={styles.modalContainer}>
+            <View style={styles.modalBox}>
+              {/* Modal Header */}
+              <View style={styles.modalHeader}>
+                <Text style={styles.modalTitle}>Make Call</Text>
+                <TouchableOpacity onPress={toggleModal}>
+                  {/* Use MaterialCommunityIcons for close icon */}
+                  <MaterialCommunityIcons
+                    name="close"
+                    size={30}
+                    color="black"
+                    style={styles.closeIcon}
+                  />
+                </TouchableOpacity>
+              </View>
+              {/* Modal Content */}
+              <View style={styles.modalContent}>
+                {/* Input Fields */}
+                <View style={styles.inputBox}>
+                  <TextInput
+                    style={styles.phoneNumberInput}
+                    placeholder="Enter Phone Number"
+                    keyboardType="phone-pad"
+                    placeholderTextColor={'purple'}
+                    onChangeText={(text) => setPhoneNumber(text)}
+                    value={phoneNumber}
+                  />
+                  <TouchableOpacity onPress={handleCallPress}>
+                    {/* Use MaterialCommunityIcons here */}
+                    <MaterialCommunityIcons
+                      name="phone"
+                      size={30}
+                      color="black"
+                      style={styles.callIcon}
                     />
-
-                    {/* Text area for comment */}
-                    <TextInput
-                        style={styles.commentInput}
-                        placeholder="Call purpose"
-                        placeholderTextColor={"purple"}
-                        multiline
-                        numberOfLines={6}
-                        onChangeText={text => setComment(text)}
-                        value={comment}
-                    />
-
-                    <Button title="Call Now" onPress={handleCallPress} color={'purple'} />
+                  </TouchableOpacity>
                 </View>
+                <TextInput
+                  style={styles.commentInput}
+                  placeholder="Call purpose"
+                  placeholderTextColor={'purple'}
+                  multiline
+                  numberOfLines={6}
+                  onChangeText={(text) => setComment(text)}
+                  value={comment}
+                />
+              </View>
             </View>
-        </ImageBackground>
-    );
+          </View>
+        </Modal>
+      </View>
+    </ImageBackground>
+  );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+      flex: 1,
+    },
+      headingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
+      
+        borderBlockColor:"black",
+        margin: 10,
+      },
+      headingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+    mainContainer: {
+      backgroundColor: 'rgba(255,255,255,0.7)',
+      padding: 10,
+      margin: 10,
+      borderRadius: 10,
     },
-    content: {
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
-        padding: 20,
-        borderRadius: 10,
-        width: "100%",
-        height: "60%",
-        alignContent: "center",
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center"
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+      backgroundColor: 'purple',
+      padding: 10,
+      borderRadius: 10,
     },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-        color: 'purple',
+    headerText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    inputBox: {
+      flexDirection: 'row',
+      marginBottom: 20,
+      alignItems: 'center',
     },
     phoneNumberInput: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 5,
-        color: "purple",
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      padding: 5,
+      color: 'purple',
+      flex: 1,
+    },
+    callIcon: {
+      width: 30,
+      height: 30,
+      marginLeft: 10,
     },
     commentInput: {
-        height: 100, // Adjust the height as needed
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 5,
-        color: "purple",
+      height: 100,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      padding: 5,
+      color: 'purple',
     },
-    logo: {
-        width: 200, // Set the desired width
-        height: 100, // Set the desired height
-        marginBottom: 100, // Adjust the spacing as needed
-        resizeMode: 'contain', // Use 'contain' to fit the image properly without cropping
-        alignSelf: 'center', // Center the image horizontally
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
-});
+    modalBox: {
+      backgroundColor: 'rgba(255, 255, 255, 0.9)',
+      borderRadius: 10,
+      padding: 20,
+      width: '80%',
+    },
+    modalHeader: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 20,
+      backgroundColor: 'purple',
+      padding: 10,
+      borderRadius: 10,
+    },
+    modalTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    modalContent: {
+      flex: 1,
+    },
+    closeIcon: {
+      marginLeft: 10,
+    },
+  });
+  
+ 
 
 export default ContactPage;
+
